3e4ba90780f884328ae97266259c6db42914bd43
Mahmoud Bayoumi <107598562+TheBayoumi@users.noreply.github.com>
2026-08-08 00:15:43 +0300

    feat(web): add onboarding route

---
+++ apps/web/app/onboarding/page.tsx
@@ -0,0 +1,8 @@
+import { Onboarding } from "../../components/onboarding/onboarding";
+
+export const dynamic = "force-dynamic";
+export const preferredRegion = "pdx1";
+
+export default function OnboardingPage() {
+  return <Onboarding />;
+}
